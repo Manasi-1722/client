1c1eb028823dd02b63423fdf37813532cf221a25
Manasi-1722 <88338443+Manasi-1722@users.noreply.github.com>
2023-08-09 04:03:15 +0530

    done

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,38 @@
+import { useEffect } from 'react';
+
+// components
+import NavBar from './NavBar';
+import Banner from './Banner';
+
+import {styled, Box } from '@mui/material';
+import { getProducts } from '../../redux/actions/productActions';
+import { useDispatch, useSelector } from 'react-redux';
+
+
+const Component = styled(Box)`
+    padding: 10px 10px;
+    background: #F2F2F2;
+`
+
+const Home = () => {
+
+    const { products } = useSelector(state => state.getProducts);
+    console.log(products);
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(getProducts())
+    }, [dispatch])
+
+    return (
+        <>
+            <NavBar />
+            <Component>
+                <Banner />
+            </Component>     
+        </>
+    )
+}
+
+export default Home;
